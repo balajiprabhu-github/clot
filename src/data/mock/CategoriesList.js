5f32bf979b6db130c804b9f3c015ccd8fefdecb5
balajiprabhu-github <balajiprabhu008@gmail.com>
2024-07-25 15:08:00 +0530

    data

---
+++ src/data/mock/CategoriesList.js
@@ -0,0 +1,24 @@
+const categoryData = [
+  {
+    id: "1",
+    title: "Electronics",
+    image: require("../../../assets/electronics.png"),
+  },
+  {
+    id: "2",
+    title: "Jewellery",
+    image: require("../../../assets/jewellery.png"),
+  },
+  {
+    id: "3",
+    title: "Men",
+    image: require("../../../assets/men_cloth.png"),
+  },
+  {
+    id: "4",
+    title: "Women",
+    image: require("../../../assets/women_cloth.png"),
+  },
+];
+
+export default categoryData;
