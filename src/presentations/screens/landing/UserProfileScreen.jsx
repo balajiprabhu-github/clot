24b87634349ee8bc2a761e7a55e1ef57ba464260
balajiprabhu-github <balajiprabhu008@gmail.com>
2024-07-29 17:20:52 +0530

    user profile

--- src/presentations/screens/landing/UserProfileScreen.jsx
+++ src/presentations/screens/landing/UserProfileScreen.jsx
@@ -1,11 +1,233 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useEffect, useState } from "react";
+import {
+  View,
+  Text,
+  ScrollView,
+  SafeAreaView,
+  TouchableOpacity,
+} from "react-native";
+import axios from "axios";
+import PrimaryButton from "../../components/buttons/PrimaryButton";
+import PrimaryTextInput from "../../components/text/PrimaryTextInput";
+import styles from "../../components/styles";
+import { useNavigation } from "@react-navigation/native";
+import Route from "../../../resources/routes";
+import Spacer from "../../components/Spacer";
+import KeyStore from "../../../data/local/KeyStore";
+import Labels from "../../../resources/label";
 
 const UserProfileScreen = () => {
+  const navigation = useNavigation();
+  const [user, setUser] = useState(null);
+  const [isEditing, setIsEditing] = useState(false);
+  const [userData, setUserData] = useState({
+    firstname: "",
+    lastname: "",
+    email: "",
+    phone: "",
+    address: {
+      city: "",
+      street: "",
+      number: "",
+      zipcode: "",
+    },
+  });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("https://fakestoreapi.com/users/1");
+        console.log(response.data);
+        setUser(response.data);
+        setUserData({
+          firstname: response.data.name.firstname,
+          lastname: response.data.name.lastname,
+          email: response.data.email,
+          phone: response.data.phone,
+          address: {
+            city: response.data.address.city,
+            street: response.data.address.street,
+            number: response.data.address.number,
+            zipcode: response.data.address.zipcode,
+          },
+        });
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const handleLogout = async () => {
+    try {
+      await KeyStore.delete("userToken");
+      navigation.replace(Route.SIGN_IN_SCREEN);
+      console.log("Logged out and token removed");
+    } catch (error) {
+      console.error("Error logging out", error);
+    }
+  };
+
+  const handleSave = async () => {
+    try {
+      const response = await axios.put(
+        `https://fakestoreapi.com/users/${user.id}`,
+        {
+          name: {
+            firstname: userData.firstname,
+            lastname: userData.lastname,
+          },
+          email: userData.email,
+          phone: userData.phone,
+          address: {
+            city: userData.address.city,
+            street: userData.address.street,
+            number: userData.address.number,
+            zipcode: userData.address.zipcode,
+            geolocation: user.address.geolocation, // keep the same geolocation
+          },
+          username: user.username,
+          password: user.password,
+        }
+      );
+      setUser(response.data);
+      setIsEditing(false);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>UserProfile Screen</Text>
-    </View>
+    <SafeAreaView style={styles.userProfileSafeArea}>
+      <ScrollView contentContainerStyle={styles.userProfileContainer}>
+        <Text style={styles.userProfileHeading}>Profile</Text>
+        <View style={styles.userProfileField}>
+          <Text>First Name:</Text>
+          <PrimaryTextInput
+            placeholder="First Name"
+            onTextChange={(text) =>
+              setUserData({ ...userData, firstname: text })
+            }
+            shouldHide={false}
+            maxLength={50}
+            value={userData.firstname}
+            editable={isEditing} // Pass the editable prop
+          />
+        </View>
+        <View style={styles.userProfileField}>
+          <Text>Last Name:</Text>
+          <PrimaryTextInput
+            placeholder="Last Name"
+            onTextChange={(text) =>
+              setUserData({ ...userData, lastname: text })
+            }
+            shouldHide={false}
+            maxLength={50}
+            value={userData.lastname}
+            editable={isEditing} // Pass the editable prop
+          />
+        </View>
+        <View style={styles.userProfileField}>
+          <Text>Email:</Text>
+          <PrimaryTextInput
+            placeholder="Email"
+            onTextChange={(text) => setUserData({ ...userData, email: text })}
+            shouldHide={false}
+            maxLength={50}
+            value={userData.email}
+            editable={isEditing} // Pass the editable prop
+          />
+        </View>
+        <View style={styles.userProfileField}>
+          <Text>Phone:</Text>
+          <PrimaryTextInput
+            placeholder="Phone"
+            onTextChange={(text) => setUserData({ ...userData, phone: text })}
+            shouldHide={false}
+            maxLength={15}
+            value={userData.phone}
+            editable={isEditing} // Pass the editable prop
+          />
+        </View>
+        <View style={styles.userProfileField}>
+          <Text>City:</Text>
+          <PrimaryTextInput
+            placeholder="City"
+            onTextChange={(text) =>
+              setUserData({
+                ...userData,
+                address: { ...userData.address, city: text },
+              })
+            }
+            shouldHide={false}
+            maxLength={50}
+            value={userData.address.city}
+            editable={isEditing} // Pass the editable prop
+          />
+        </View>
+        <View style={styles.userProfileField}>
+          <Text>Street:</Text>
+          <PrimaryTextInput
+            placeholder="Street"
+            onTextChange={(text) =>
+              setUserData({
+                ...userData,
+                address: { ...userData.address, street: text },
+              })
+            }
+            shouldHide={false}
+            maxLength={50}
+            value={userData.address.street}
+            editable={isEditing} // Pass the editable prop
+          />
+        </View>
+        <View style={styles.userProfileField}>
+          <Text>Number:</Text>
+          <PrimaryTextInput
+            placeholder="Number"
+            onTextChange={(text) =>
+              setUserData({
+                ...userData,
+                address: { ...userData.address, number: text },
+              })
+            }
+            shouldHide={false}
+            maxLength={10}
+            value={userData.address.number.toString()}
+            editable={isEditing} // Pass the editable prop
+          />
+        </View>
+        <View style={styles.userProfileField}>
+          <Text>Zipcode:</Text>
+          <PrimaryTextInput
+            placeholder="Zipcode"
+            onTextChange={(text) =>
+              setUserData({
+                ...userData,
+                address: { ...userData.address, zipcode: text },
+              })
+            }
+            shouldHide={false}
+            maxLength={10}
+            value={userData.address.zipcode}
+            editable={isEditing} // Pass the editable prop
+          />
+        </View>
+        <PrimaryButton
+          label={isEditing ? Labels.SAVE_CHANGES : Labels.EDIT_PROFILE}
+          onPress={isEditing ? handleSave : () => setIsEditing(true)}
+          enabled={true}
+        />
+
+        <Spacer height={20} />
+
+        <TouchableOpacity style={styles.logoutButton} onPress={handleLogout}>
+          <Text style={styles.logoutButtonText}>{Labels.LOGOUT}</Text>
+        </TouchableOpacity>
+        
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
