b5f57b43b65e903779c6077414552aee48249fa8
balajiprabhu-github <balajiprabhu008@gmail.com>
2024-07-29 20:54:02 +0530

    fav screen

--- src/presentations/screens/landing/FavouritesScreen.jsx
+++ src/presentations/screens/landing/FavouritesScreen.jsx
@@ -3,8 +3,8 @@ import { View, Text } from "react-native";
 
 const FavouritesScreen = () => {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Favourites Screen</Text>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center",backgroundColor:'white' }}>
+      <Text>Coming Soon...</Text>
     </View>
   );
 };
